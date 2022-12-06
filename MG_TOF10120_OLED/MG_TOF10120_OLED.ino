ee72f466d5528557fa5952212621ad264c72c4cd
martinglass <41255030+martinglass@users.noreply.github.com>
2022-12-06 15:44:08 +0000

    Uploaded the .ino file for Arduino App use

--- MG_TOF10120_OLED/MG_TOF10120_OLED.ino
+++ MG_TOF10120_OLED/MG_TOF10120_OLED.ino
@@ -1,4 +1,4 @@
-/* This code is to use with ToF10120 Laser Range Sensor to measure distance in (mm) and 
+/* This code is to use with ToF10120 Laser Range Sensor to measure distance in (mm) & 
  * shows it on on an OLED screen using I²c inteface and in the Serial monitor of the Arduino app 
  * Modified and adapted from https://www.electroniclinic.com/ 
  * by Martin Glass December 2022 (c)
